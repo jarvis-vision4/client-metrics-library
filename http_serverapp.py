0da4f518095318e2438847be00102fd762e8a0bb
Thurein Cloud <thureinh64@gmail.com>
2026-01-18 18:32:24 +0630

    New Project

---
+++ http_serverapp.py
@@ -0,0 +1,28 @@
+import http.server
+from prometheus_client import start_http_server
+class HandleRequests(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        html = """
+        <html>
+            <head>
+                <title>First Python Application</title>
+            </head>
+            <body style="color:#333; margin-top:30px;">
+                <center>
+                    <h2>Welcome to our first Python application.</h2>
+                </center>
+            </body>
+        </html>
+        """
+
+        self.wfile.write(html.encode("utf-8"))
+
+if __name__ == "__main__":
+    start_http_server(8001)
+    server = http.server.HTTPServer(('0.0.0.0', 8000), HandleRequests)
+    server.serve_forever()
